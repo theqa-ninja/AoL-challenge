5f9f3a633a1b47f087e56d44568fdcb94fd6b5c9
QA ninja <qa_ninja@live.com>
2023-11-17 13:00:43 -0600

    forgot to add json file

--- test_subscription.py
+++ test_subscription.py
@@ -1,11 +1,19 @@
 from faker import Faker
 from playwright.sync_api import Page, expect
+import json
 
 fake = Faker()
 
+with open('test_data.json') as data_file:
+    file_contents = data_file.read()
+
+data = json.loads(file_contents)
+
+import pdb; pdb.set_trace()
+
 def test_SignUp(page: Page):
     # Go to https://www.abcmouse.com
-    page.goto("https://www.abcmouse.com")
+    page.goto(data["starting_url"])
 
     # If we get "Please verify you are a human" check on h1 tag, fail
     expect(page.get_by_role("heading", level=1, name="Please verify you are a human")).not_to_be_visible(timeout=5000)
@@ -15,11 +23,15 @@ def test_SignUp(page: Page):
 
     # Verify that  https://www.abcmouse.com/abt/register page is returned
     # changed this to https://www.abcmouse.com/abc/prospect-register/ per the manual test of this page
-    expect(page).to_have_url("https://www.abcmouse.com/abc/prospect-register/")
+    expect(page).to_have_url(data["signup_url"])
 
     # Enter Email address (any email address)
     # generate fake e-mail
     email = fake.email()
+
+    # if we really want to load then uncomment next line
+    # email = data["email"]
+
     # locate & fill out by aria-label
     page.get_by_label("Edit box, Email Address, Enter your email address. This is a required field.").fill(email, timeout=5000)
 
@@ -27,7 +39,7 @@ def test_SignUp(page: Page):
     page.get_by_role("button", name="Submit your email address to begin.").click(timeout=5000)
 
     # Verify that https://www.abcmouse.com/abt/subscription page is returned.
-    expect(page).to_have_url("https://www.abcmouse.com/abc/subscription/")
+    expect(page).to_have_url(data["submitted_url"])
 
     # Verify that on subscription page, “Become a Member!” text is rendered.
     # verify the e-mail entered on the last page matches the one displayed here
